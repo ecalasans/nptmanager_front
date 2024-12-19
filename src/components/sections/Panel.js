88ad69a8e8d0998290ed1d15e174ef9b003b36bf
ecalasans <ericcalasans@gmail.com>
2024-12-19 06:21:30 -0300

    Start creating Panel component

---
+++ src/components/sections/Panel.js
@@ -0,0 +1,12 @@
+import "./css/sections.css"
+import {Container} from "react-bootstrap";
+
+const Panel = () => {
+    return (
+        <Container fluid className="w-100 h-100">
+
+        </Container>
+    )
+}
+
+export default Panel;
